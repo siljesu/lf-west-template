e0a121484f45fa2429cc4dd966a59f17bb32bc31
Silje Susort <silje.susort@gmail.com>
2023-02-23 10:33:21 +0100

    Add west build command for federated execution

--- scripts/lf_build.py
+++ scripts/lf_build.py
@@ -1,6 +1,7 @@
 from west.commands import WestCommand  # your extension must subclass this
 from west import log                   # use this for user output
 
+import os
 import subprocess
 
 class LfBuild(WestCommand):
@@ -92,3 +93,101 @@ class LfBuild(WestCommand):
         ret = res.wait()
         if ret != 0:
             exit(1)
+            
+class LfFedBuild(WestCommand):
+    def __init__(self):
+        super().__init__(
+            'lf-fed-build',               # gets stored as self.name
+            'Compile LF program and then run west build, tweaked for federated',  # self.help
+            ""
+        )
+        # To use a specific lfc binary the following variable can be modified
+        # or the path to the desired binary can be passed to `--lfc`
+        self.lfcPath = "lfc"
+
+    def do_add_parser(self, parser_adder):
+        # This is a bit of boilerplate, which allows you full control over the
+        # type of argparse handling you want. The "parser_adder" argument is
+        # the return value of an argparse.ArgumentParser.add_subparsers() call.
+        parser = parser_adder.add_parser(self.name,
+                                         help=self.help,
+                                         description=self.description)
+
+        # Add some example options using the standard argparse module API.
+        # parser.add_argument('-o', '--optional', help='an optional argument')
+        # parser.add_argument('project_root', help='Path to root of project')
+        parser.add_argument('main_lf', help='Name of main LF file')
+        parser.add_argument('-w', '--west-commands', help='Arguments to forward to west')
+        parser.add_argument('-c', '--conf-overlays', help='Additional configuration overlays')
+        parser.add_argument('--lfc', help='Path to LFC binary')
+
+        return parser           # gets stored as self.parser
+
+    def do_run(self, args, unknown_args):
+        
+        if "src" not in args.main_lf.split("/"):
+            print("ERROR: West lf-fed-build must be invoked outside `src` folder")
+            
+        srcGenPath = args.main_lf.split(".")[0].replace("src", "fed-gen")+"/src-gen"
+        print(f'srcGenPath: {srcGenPath}')
+        print(f'srcGenPath: {"/home/silje/lf-west-app/application/"+srcGenPath}')
+
+        appPath = args.main_lf.split("src")[0]
+        if appPath == "":
+            appPath = "."
+
+        # 1. Invoke lfc with clean flag `-c` and without invoking target
+        #    compiler `n`.
+        if args.lfc:
+            self.lfcPath = args.lfc
+        
+        lfcCmd = f"{self.lfcPath} -c -n {args.main_lf}"
+        print(f"Executing lfc command: `{lfcCmd}`")
+        res = subprocess.Popen(lfcCmd, shell=True)
+        ret = res.wait()
+        if ret != 0:
+            exit(1)
+
+        if not args.west_commands:
+            args.west_commands = ""
+        
+        # FIXME: This is a not-intuitive limitation from the users prespective.
+        # But we use `-DOVERLAY_CONFIG` to mix in the prj.conf from the app
+        # directory with the prj_lf.conf in the `src-gen`
+        if "-DOVERLAY_CONFIG" in args.west_commands:
+            print("Error: Use `--conf-overlays` option to pass config overlays to west")
+
+        federateNames = [name for name in os.listdir("./"+srcGenPath)]
+	
+        for fedName in federateNames:
+            # Copy project configurations into src-gen 
+            userConfigPaths="prj.conf"
+            res = subprocess.Popen(f"cp {appPath}/prj.conf {srcGenPath}/{fedName}/", shell=True)
+            if args.conf_overlays:
+                res = subprocess.Popen(f"cp {appPath}/{args.conf_overlays} {srcGenPath}/{fedName}/", shell=True)
+                userConfigPaths += f";{args.conf_overlays}"
+
+            # Copy the Kconfig file into the src-gen directory
+            res = subprocess.Popen(f"cp {appPath}/Kconfig {srcGenPath}/{fedName}/", shell=True)
+            ret = res.wait()
+            if ret != 0:
+                exit(1)
+
+            # Parse the generated CompileDefinitions.txt which should be 
+            # forwarded to west
+            compileDefs = ""
+            with open(f"{srcGenPath}/{fedName}/CompileDefinitions.txt") as f:
+                for line in f:
+                    line = line.replace("\n", "")
+                    compileDefs += f"-D{line} "
+
+        # Let file copying finish before attempting to build            
+        for fedName in federateNames:
+            print(compileDefs)
+            # Invoke west in the `src-gen` directory. Pass in 
+            westCmd = f"west build {srcGenPath}/{fedName} {args.west_commands} -- -DOVERLAY_CONFIG=\"{userConfigPaths}\" {compileDefs}"
+            print(f"Executing west command: `{westCmd}`")
+            res = subprocess.Popen(westCmd, shell=True)
+            ret = res.wait()
+            if ret != 0:
+                exit(1)
